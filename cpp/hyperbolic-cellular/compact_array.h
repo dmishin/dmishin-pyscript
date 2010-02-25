023fd2417232347ad8bdacef4a2e3bc718a907ea
Dmishin <shintyakov@gmail.com>
2010-02-25 22:28:52 +0300

    Fixed compact array

--- cpp/hyperbolic-cellular/compact_array.h
+++ cpp/hyperbolic-cellular/compact_array.h
@@ -2,6 +2,8 @@
 #ifndef __COMPACT_ARRAY__
 #define __COMPACT_ARRAY__
 
+#include <cassert>
+
 const int BYTE_SIZE = 8;//oh well.
 
 #define DIVIDE_UP(a,b) ((a)%(b)==0?((a)/(b)):((a)/(b)+1))
@@ -12,29 +14,46 @@ class compact_array
 public:
     typedef unsigned int piece_type;
 
-    static const int items_in_piece = sizeof(piece_type)/item_bits;
+    typedef piece_type value_type; //for STL compatibility
+
+    static const int items_in_piece = (BYTE_SIZE*sizeof(piece_type))/item_bits;
     static const int num_pieces = DIVIDE_UP( num_items, items_in_piece );
     static const piece_type item_mask = (piece_type(1)<<item_bits)-1;
 
     static const int used_bits = item_bits*items_in_piece;
     static const piece_type used_bits_mask = ~((~piece_type(0))<<used_bits);
+    
+    static const value_type max_value = item_mask;
 
     piece_type data[num_pieces];
+
+    int size()const{
+	return num_items;
+    };
+
     piece_type get(int idx)const{
+	assert(idx >= 0 && idx < num_items);
+
 	int piece_idx = idx/items_in_piece;
-	int offset = item_bits*(piece_idx%items_in_piece);
+	int offset = item_bits*(idx%items_in_piece);
 	return (data[piece_idx]>>offset)&item_mask;
     };
+
     void set(int idx, piece_type val){
+	assert(idx >= 0 && idx < num_items);
+	assert( (val & ~item_mask) == 0); //value has no bits outside mask
 	int piece_idx = idx/items_in_piece;
-	int offset = item_bits*(piece_idx%items_in_piece);
+	int offset = item_bits*(idx%items_in_piece);
 	data[piece_idx] = (data[piece_idx] & ~(item_mask<<offset)) | (val<<offset);
     };
+
+    /** clear with zeros */
     void zero(){
 	for(int i = 0;i<num_pieces;++i){ data[i]=0; };
     }
-    struct item_setter{
 
+    /*Proxy object for implementing the [] operator*/
+    struct item_setter{
 	int idx;
 	compact_array &array;
 
@@ -42,17 +61,23 @@ public:
 	    :idx(_idx),array(arr)
 	    {};
 	
-	operator piece_type(){
+	operator piece_type()const{
 	    return array.get( idx );
 	};
 	item_setter& operator=(const piece_type& v){
 	    array.set( idx, v);
 	    return *this;
-	}; 
+	};
     };
-    item_setter operator[](int idx){
+
+    item_setter operator[](int idx){ //return special proxy object, imitating reference
 	return item_setter(*this, idx);
     };
+    piece_type operator[](int idx)const{
+	return get(idx);
+    };
+
+    //TODO: implement an STL iterator interface?
 };
 
 #endif
--- cpp/hyperbolic-cellular/test_carray.cpp
+++ cpp/hyperbolic-cellular/test_carray.cpp
@@ -3,11 +3,25 @@
 
 int main(int argc, char* argv[])
 {
-    compact_array<10, 2> array;
-    array[3]=1;
-    std::cout<<"item mask:"<<array.item_mask<<std::endl;
-    std::cout<<array[3]<<std::endl;
-    std::cout<<array.items_in_piece<<std::endl;
+    std::cout<<"Testing array\n";
+    compact_array<10, 13> array;
+    compact_array<12,3> array_anouther;
+    for (int i =0; i<array.size(); ++i){
+//	array.set(i, i%3);
+	array_anouther[i] = 3;
+	array[i] = i%3;
+    }
+
+    compact_array<10,13> array1;
+    array1 = array;
+
+    std::cout<<"Array item mask:"<<array.item_mask<<std::endl;
+    std::cout<<"Array num_pieces:"<<array.num_pieces<<std::endl;
+    std::cout<<"Items in piece:"<<array.items_in_piece<<std::endl;
+    for(int i=0; i<array1.size(); ++i){
+	std::cout<<"["<<array1.get(i)<<"]";
+    }
+    std::cout<<std::endl;
     std::cout<<"done"<<std::endl;
     return 0;
 }
