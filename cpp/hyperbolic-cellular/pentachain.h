9cf65fa9921d1315de099e99d6eeea295d7c18ea
Dmishin <shintyakov@gmail.com>
2010-02-25 23:20:02 +0300

    Nifty version of PentaChain

--- cpp/hyperbolic-cellular/pentachain.h
+++ cpp/hyperbolic-cellular/pentachain.h
@@ -1,87 +1,96 @@
 #pragma once
 #ifndef __PENTACHAIN_INCLUDED__
 #define __PENTACHAIN_INCLUDED__
+#include <cassert>
 
-#include "bitchain.h"
+#include "compact_array.h"
 #include <string>
 #include <stdexcept>
 
 class PentaChain{
 public:
-    bitchain chain;
+    static const int MAX_CHAIN=100;
+
+    typedef compact_array<MAX_CHAIN, 3> ArrayType; //type of the internal compact array
+    ArrayType chain;
+    int chainLen;
+
     typedef enum{
 	Node2_0, Node2_1,
 	Node3_0, Node3_1, Node3_2
     } ChainElement;
+
     typedef enum{
-	Node2, Node3
+	Node2, Node3, __WRONG_NODE_TYPE
     } NodeType;
 
-    ChainElement get( int idx)const{
-	int val = chain[idx];
-	if (getNodeType( idx )== Node2){
-	    if (val >=0 && val < 2)
-		return static_cast<ChainElement>(Node2_0+val);
-	}else{
-	    if (val>=0 && val<3)
-		return static_cast<ChainElement>(Node3_0+val);
-	};
-	throw std::logic_error("Failed to get(): chain contains incorrect item");
+    PentaChain()
+	: chainLen(0)
+    { 
+	chain.zero();//zero-init chain for simplier comparision;
+    }
+    
+    
+    ChainElement get( int idx )const{
+	assert( idx>=0 && idx < chainLen);
+	ArrayType::value_type val = chain[idx];
+	assert(val <= Node3_2);
+	return static_cast<ChainElement>( val  );
     };
 
-    int size()const{ return chain.size();};
-    void set(int idx, ChainElement v);
-    
-    NodeType getNodeType( int idx )const{
-	if ( idx == 0 || idx == -1)
-	    return Node3;//start node is 3-node
-	else{
-	    //check previous value
-	    ChainElement prevElt = get( idx-1 );
-	    if ( prevElt==Node2_1 || prevElt == Node3_2)
-		return Node2;
-	    else
-		return Node3;
-	}
+    ChainElement get()const{
+	return get(size()-1);
     }
-    NodeType getNodeType()const{ 
-	return getNodeType(size()-1); 
-    };
+
+    int size()const{
+	return chainLen;
+    }
+    
+    /**Returns type of the new node to be created*/
+    NodeType getNewNodeType()const{
+	if (size()==0) return Node3;//0th noded is always 3-node
+	ChainElement last = get();
+	if (last == Node2_0 || last == Node3_0) //TODO: check this!
+	    return Node2;
+	else
+	    return Node3;
+    }
+
+    /**Get type of the existing node*/
+    NodeType getNodeType( int idx )const{
+	return nodeType( get(idx) );
+    }
+
+    /**Return the node type of the element*/
+    static NodeType nodeType( ChainElement elt ){
+	if ( elt >= Node2_0 && elt <=Node2_1)
+	    return Node2;
+	else if(elt >= Node3_0 && elt <=Node3_2)
+	    return Node3;
+	else
+	    assert( false );//Wrong node value. Memory corruption?
+	return __WRONG_NODE_TYPE;//dummy for compiler.
+    }	
+
+    /** append an element. Note, that setting element inside is almost always impossible!*/
     void append(ChainElement e){
-	unsigned int val = 0;
-	if (getNodeType() == Node2){
-	    switch( e ){
-		case Node2_0: 
-		    val = 0; break;
-		case Node2_1:
-		    val = 1; break;
-		default:
-		    throw std::logic_error("Wrong node type");
-	    }
-	}else{
-	    switch( e ){
-		case Node3_0: 
-		    val = 0; break;
-		case Node3_1:
-		    val = 1; break;
-		case Node3_2:
-		    val = 2; break;
-		default:
-		    throw std::logic_error("Wrong node type");
-	    }
-	}
-	chain.append( val );
-    };
+	assert( getNewNodeType() == nodeType( e ) );//node type must be right!
+	chain[ chainLen ] = e;
+	chainLen ++;
+    }
+
     ChainElement pop(){
-	ChainElement rval = get(size()-1);
-	chain.pop();
+	ChainElement rval = get();
+	chain.set( chainLen-1, 0);//clear the value, to simplify comparisions
+	chainLen--;
 	return rval;
     };
+    
     std::string toStr()const{
 	std::string rval;
 	
 	for(int i=0;i<size();++i){
-	    char* c="";
+	    const char* c="";
 	    switch (get(i)){
 		case Node2_0:
 		    c="[0/2]";break;
--- cpp/hyperbolic-cellular/test_pentachain.cpp
+++ cpp/hyperbolic-cellular/test_pentachain.cpp
@@ -6,9 +6,12 @@ int main(int argc, char* argv[])
     try{
 	PentaChain c;
 	c.append(PentaChain::Node3_1);
+	c.append(PentaChain::Node3_0);
+	c.append(PentaChain::Node3_1);
 
 	std::cout<<"Hello\n";
 	std::cout<<"{"<<c.toStr()<<"}"<<std::endl;
+
     }catch(std::logic_error e){
 	std::cerr<<e.what()<<std::endl;
     };
