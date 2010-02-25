8c4de8b5c9d968f0ba93091c72af0e797a319e9d
Dmishin <shintyakov@gmail.com>
2010-02-25 22:41:36 +0300

    Comments for the compact array code

--- cpp/hyperbolic-cellular/compact_array.h
+++ cpp/hyperbolic-cellular/compact_array.h
@@ -2,11 +2,26 @@
 #ifndef __COMPACT_ARRAY__
 #define __COMPACT_ARRAY__
 
+/*********************************************************************************
+ *  Memory-efficient, fixed size container for values with small number of bits.
+ *  Usable, when you need a compact array of values, which are shorter than byte. (1-7 bits).
+ * 
+ *
+ *  This container implements efficient packing of short values into an array-like container
+ *  Item size in bits must not be more than size of integer value (for wider values, compact
+ *  storage is not a consideration). For the sake of speed, this class does not splits one 
+ *  value on two words, thus, for 3-bit values, only 30 bits of every word are used on 32-bit machine.
+ *
+ * Dmishin. This code is free domain.
+ ***********************************************************************************/
+
+
 #include <cassert>
 
 const int BYTE_SIZE = 8;//oh well.
 
-#define DIVIDE_UP(a,b) ((a)%(b)==0?((a)/(b)):((a)/(b)+1))
+//convenience macro for integer divisin with rounding up. Used for constant initialization.
+#define CARRAY_DIVIDE_UP(a,b) ((a)%(b)==0?((a)/(b)):((a)/(b)+1))
 
 template<int num_items, int item_bits>
 class compact_array
@@ -17,12 +32,12 @@ public:
     typedef piece_type value_type; //for STL compatibility
 
     static const int items_in_piece = (BYTE_SIZE*sizeof(piece_type))/item_bits;
-    static const int num_pieces = DIVIDE_UP( num_items, items_in_piece );
+    static const int num_pieces = CARRAY_DIVIDE_UP( num_items, items_in_piece );
     static const piece_type item_mask = (piece_type(1)<<item_bits)-1;
 
     static const int used_bits = item_bits*items_in_piece;
     static const piece_type used_bits_mask = ~((~piece_type(0))<<used_bits);
-    
+    /**Maximal possible value(inclusive)*/
     static const value_type max_value = item_mask;
 
     piece_type data[num_pieces];
@@ -73,6 +88,7 @@ public:
     item_setter operator[](int idx){ //return special proxy object, imitating reference
 	return item_setter(*this, idx);
     };
+
     piece_type operator[](int idx)const{
 	return get(idx);
     };
