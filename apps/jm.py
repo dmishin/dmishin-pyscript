117163fce5d07189b8f9aefef2f87d1164d97787
Dmishin <shintyakov@gmail.com>
2010-02-27 22:17:43 +0300

    before merge

--- apps/jm.py
+++ apps/jm.py
@@ -6,7 +6,6 @@ create table instance( id integer primary key, media_id, path);
 create table media(id integer primary key, default_mount_point, mount_point, is_removable, name);
 create table tags (id_item, id_tag, value);
 create table tag (id integer primary key, name);
-
 """
 
 database = ":memory:"
@@ -19,28 +18,39 @@ class JM:
     def connect(self, database):
         "Conenct to the database"
         self.connection = sql.connect( database )
-        self.cursor = self.connection.cursor
+        self.cursor = self.connection.cursor()
 
         self.check_database()
     
     def check_database(self ):
         "Ensure, that database has neede structure"
         self.cursor.execute("select (name) from sqlite_master where type='table' and name='item'")
-        if len(self.cursor) == 0:
-        #no databases
+        if not self.cursor == 0:#FIXME
+            #no databases
             print "Initializing database"
-            self.cursor.execute_script( SQL )
+            self.connection.executescript( SQL )
 
 
     def get_tag(self, tag_name, create_new = False ):
         "Get tag ID and register new tag, if there is no such tag"
+        self.cursor.execute("select (id) from tag where name=?", (tag_name,))
+        if not self.cursor == 0:#FIXME
+            print "Tag %s not found"%tag_name
+            self.cursor.execute("insert into tag(name) values (?)", (tag_name,))
+            return self.cursor.lastrowid
+        return list(self.cursor)
         pass
 
     def get_item(self, path):
         apath = os.path.abspath( path )
         if not os.path.exists( path ):
             raise ValueError, "File %s not found"%path
-#find media
+        #find media
         media = self.find_media( apath )
 
 
+jm = JM( database )
+
+print "Getting tag test twice"
+print jm.get_tag("test")
+print jm.get_tag("test")
