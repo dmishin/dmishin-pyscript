5b894cd525fc5cea4d942fedddef2f8dfe0f2607
Dmi Shin <shintyakov@gmail.com>
2010-04-01 18:28:42 +0400

    Self-optimization

--- apps/my-optimization.py
+++ apps/my-optimization.py
@@ -1,4 +1,5 @@
-
+import psyco
+psyco.full()
 
 import numpy
 
@@ -11,19 +12,24 @@ def show( *msgs ):
 
 
 
-def contracting_simplex( initial_points, func, ke=2.0, ks=0.5, kgs=0.5, max_steps=10000, eps=1e-5 ):
+def contracting_simplex( initial_points, func, ke=2.0, ks=0.5, kgs=0.5, max_calculations=10000, eps=1e-5  ):
     N = len(initial_points[0])
     assert( N+1 == len(initial_points) )
 
     steps = 0
+    calculations = [0]
+    def cfunc( v ): #accounting number of function calls
+        calculations[0] += 1
+        return func( v )
 
     #calculate values
-    slots = [ [point, func(point)] for point in initial_points ]
+    slots = [ [point, cfunc(point)] for point in initial_points ]
+    
 
     while True:
         steps += 1
-        if steps > max_steps:
-            show( "Max steps reached")
+        if calculations[0] > max_calculations:
+            show( "Max calls reached")
             break
         #Sort the items by the value
         slots.sort( key=lambda slot:slot[1] )
@@ -36,12 +42,12 @@ def contracting_simplex( initial_points, func, ke=2.0, ks=0.5, kgs=0.5, max_step
         show( "Center:", xc)
         #reflect
         xr = xc*2-x_worst
-        fr = func( xr )
+        fr = cfunc( xr )
 
         if fr < f_best:
             #very good, expand
             xe = xc+ ke*(xr-xc)
-            fe = func(xe )
+            fe = cfunc(xe )
             if fe < fr:
                 show( "expand success")
                 slots[-1]=[xe, fe]
@@ -52,7 +58,7 @@ def contracting_simplex( initial_points, func, ke=2.0, ks=0.5, kgs=0.5, max_step
             #at least better
             #contract
             xs = xc + ks*(x_worst-xc)
-            fs = func( xs )
+            fs = cfunc( xs )
             if fs < fr:
                 show( "contract...")
                 slots[-1] = [xs, fs]
@@ -63,14 +69,14 @@ def contracting_simplex( initial_points, func, ke=2.0, ks=0.5, kgs=0.5, max_step
             #global contract
             show( "global contract")
             new_points = [(slot[0]-xc)*kgs+xc for slot in slots]
-            slots = [[point, func( point )] for point in new_points]
+            slots = [[point, cfunc( point )] for point in new_points]
             vals = [slot[1] for slot in slots]
             if max(vals)-min(vals) < eps:
                 show( "found")
                 show( slots)
                 break
     slots.sort( key = lambda slot:slot[1] )
-    return steps, slots
+    return calculations[0], slots
 
 def rozen(v):
     x,y=v
@@ -81,11 +87,62 @@ def rozen(v):
 #     lambda v: (v[0]-5)**2+2*(v[1]-3)**4
 #     )
 
+
+def test_suite( inital_points_set, func, eps, answer, max_calculations=10000, answer_eps=1e-2):
+    
+    def tester(ke, ks, kgs):
+        n_failures = 0
+        n_calculations = 0
+        for points in inital_points_set:
+            calc, slots = contracting_simplex( points, func, ke=ke, ks=ks, kgs=kgs, eps=eps, max_calculations = max_calculations)
+            x, fmin = slots[0]
+            d = numpy.max(numpy.abs(x-answer))
+            if d>answer_eps:
+                #Mistaks
+                n_failures += 1
+            else:
+                n_calculations += calc
+        return n_calculations, n_failures
+    return tester
+
+
+
+
 steps, slots = contracting_simplex(
     map(numpy.array, [ [-1,0], [0,5], [1,0] ] ),
     rozen,
-    eps=1e-12
+    eps=1e-14
     )
 
 print "Steps:", steps
 print "Solution:", slots[0]
+
+
+#make initial points
+from random import random
+n_sets = 1000
+def rvec2():
+    return numpy.array([random(), random()])*10-5
+
+point_set = [[rvec2() for j in xrange(3)] for  i in xrange(n_sets)]
+    
+tester = test_suite( point_set, rozen, 1e-12, numpy.array([1,1]))
+
+def ofun( k_esgs):
+    ke, ks, kgs = k_esgs
+    steps, errors = tester( ke, ks, kgs )
+    return steps/float(len(point_set)-errors)
+
+#trying to find optimal parameters for ofun
+
+print "Optimizing parameters"
+
+steps, slots = contracting_simplex(
+    map(numpy.array, 
+        [ [2,0.5,0.5], [2.1, 0.5, 0.6], [2.2, 0.3,0.5], [1.9, 0.5, 0.4] ] ),
+    ofun,
+    eps=0.5,
+    max_calculations = 300
+    )
+
+print steps, slots
